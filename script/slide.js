0eceda9efcfd3650300ef7fc9f22a91a3ceeae9b
Prajwal Bajracharya <bajracharya1994@gmail.com>
2017-12-12 17:59:35 +0545

    major changes to design, including nav dropdown contents and basic animation

--- script/slide.js
+++ script/slide.js
@@ -8,3 +8,14 @@ $(window).scroll(function() {
     }
   });
 });
+
+$(window).scroll(function() {
+  $(".fadeanim").each(function(){
+    var pos = $(this).offset().top;
+
+    var winTop = $(window).scrollTop();
+    if (pos < winTop + 600) {
+      $(this).addClass("appear");
+    }
+  });
+});
